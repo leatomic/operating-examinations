c14a6b59f62686f97708b9c5d12f4c70bf1c030c
leatomic <le.atomic@outlook.com>
2019-05-26 00:05:03 +0800

    rename class com.lklk.examination.LongestSequenceFinderOfRepeatedChars.Result to ...LongestSequences

--- src/com/lklk/examination/LongestSequenceFinderOfRepeatedChars.java
+++ src/com/lklk/examination/LongestSequenceFinderOfRepeatedChars.java
@@ -25,31 +25,33 @@ public class LongestSequenceFinderOfRepeatedChars {
 
 
 
-    public Result findFrom(String target) {
+    public LongestSequences findFrom(String target) {
 
         if (target == null || target.isEmpty()) {
             throw new IllegalArgumentException("target string cannot be null nor empty");
         }
 
-        Result result = new Result(target);
-//        var result = new ResultWithoutDuplicateCharSequence(target);
-
         char[] chars = target.toCharArray();
+        LongestSequences longestSequences = new LongestSequences(target);
+//        var longestSequences = new LongestSequencesWithoutDuplicate(target);
+
 
         if (chars.length == 1) {
-            result.accept(0, 1);
-            return result;
+            longestSequences.accept(0, 1);
+            return longestSequences;
         }
 
 
-        // 假设第一个字母已被统计过了
+        // 假设第一个字母已被遍历过了
         char charOfLastSeq = chars[0];
         int countOfLastSeq = 1, beginIdxOfLastSeq = 0;
 
-        // 从第二个字母开始
+        /* 从第二个字母开始往后遍历，
+         * 若发现与上一个字符charOfLastSeq不一致，说明从beginIdxOfLastSeq的位置开始，截至charOfLastSeq的位置为止可算作一个序列
+         * */
         for (int i = 1; i < chars.length; i++) {
             if (chars[i] != charOfLastSeq) {
-                result.accept(beginIdxOfLastSeq, countOfLastSeq);
+                longestSequences.accept(beginIdxOfLastSeq, countOfLastSeq);
                 charOfLastSeq = chars[i];
                 beginIdxOfLastSeq = i;
                 countOfLastSeq = 1;
@@ -58,11 +60,9 @@ public class LongestSequenceFinderOfRepeatedChars {
                 countOfLastSeq ++;
             }
         }
-
         // 还需检测最后一个序列是否是最长的
-        result.accept(beginIdxOfLastSeq, countOfLastSeq);
-
-        return result;
+        longestSequences.accept(beginIdxOfLastSeq, countOfLastSeq);
+        return longestSequences;
     }
 
 
@@ -71,22 +71,22 @@ public class LongestSequenceFinderOfRepeatedChars {
 
 
 
-    public static class Result {
+    public static class LongestSequences {
 
         private final String target;
 
         private List<Integer> beginIndexes = new ArrayList<>();     // 有需要的话可根据target字符串中该下标对应的字符进行去重
         private int length;                                         // 当前最长序列的长度
 
-        private Result(String target) {
+        private LongestSequences(String target) {
             this.target = target;
         }
 
         /**
          * <p>检测目标字符串中，以 {@code beginIdxOfSeq} 为起始下标，长度为 {@code length} 的字符序列，并对当前结果做增量更新
          * <ol>
-         *  <li> 如果其长度比所有当前检测过的字符序列的都要长则只保留该序列，并刷新{@link Result#length}；
-         *  <li> 如果其长度刚好等于{@link Result#length}，则将其保存到当前结果中；
+         *  <li> 如果其长度比所有当前检测过的字符序列的都要长则只保留该序列，并刷新{@link LongestSequences#length}；
+         *  <li> 如果其长度刚好等于{@link LongestSequences#length}，则将其保存到当前结果中；
          *  <li> 否则无需做任何操作。
          * </ol>
          */
@@ -118,14 +118,14 @@ public class LongestSequenceFinderOfRepeatedChars {
 
 
 
-    public static class ResultWithoutDuplicateCharSequence {
+    public static class LongestSequencesWithoutDuplicate {
 
         private final String target;
 
         private Map<Character, Integer> charBeginIdxMap = new HashMap<>();
         private int length;
 
-        private ResultWithoutDuplicateCharSequence(String target) {
+        private LongestSequencesWithoutDuplicate(String target) {
             this.target = target;
         }
 
